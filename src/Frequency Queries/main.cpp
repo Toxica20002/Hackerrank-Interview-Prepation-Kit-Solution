402fd004ff3fc0018fb6082e675823eae7e96c81
YinLin <42279296+tox480@users.noreply.github.com>
2022-04-30 22:20:34 +0700

    Create main.cpp

---
+++ src/Frequency Queries/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, a, b) for((i) = a; (i) < (b); ++(i))
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    cin.exceptions(std::istream::failbit);
+    int qu; cin >> qu;
+    unordered_map<ll, int, custom_hash> dic, freq;
+    while(qu--){
+        int q; cin >> q;
+        if(q== 1){
+            int x; cin >> x;
+            if(!dic.count(x) || !dic[x]){
+                dic[x]= 1;
+                freq[1]++;
+            }
+            else{
+                freq[dic[x]]--;
+                dic[x]++;
+                freq[dic[x]]++;
+            }
+        }
+        else if(q== 2){
+            int x; cin >> x;
+            if(!dic.count(x) || !dic[x])
+                continue;
+            else{
+                freq[dic[x]]--;
+                dic[x]--;
+                if(!dic[x]) continue;
+                freq[dic[x]]++;
+            }
+        }
+        else{
+            int x; cin>> x;
+            if(freq.count(x) && freq[x]!= 0) {
+                cout << 1 << "\n";
+            }
+            else cout << 0 << '\n';
+        }
+    }
+    return 0;
+}
